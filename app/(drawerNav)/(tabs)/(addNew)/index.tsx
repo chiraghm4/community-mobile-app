3c11011d840a4f4e50ca11d24a912de0e17cb31d
chiraghm4 <chiragmaski4@gmail.com>
2025-01-30 20:47:08 +0530

    add new page completed logic - styling left

--- app/(drawerNav)/(tabs)/(addNew)/index.tsx
+++ app/(drawerNav)/(tabs)/(addNew)/index.tsx
@@ -1,21 +1,55 @@
-import { Button, Pressable, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import {
+  Button,
+  Pressable,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { supabase } from "@/utils/supabase";
 import * as ImagePicker from "expo-image-picker";
 import { useEffect, useState } from "react";
 import { getAuth } from "firebase/auth";
-import {decode} from 'base64-arraybuffer'
+import { decode } from "base64-arraybuffer";
 import { pushSupabaseURL2Firebase } from "@/helpers/hSupabaseImageHandler";
-import RNPickerSelect from 'react-native-picker-select'
+import RNPickerSelect from "react-native-picker-select";
 import { TextInput } from "react-native-gesture-handler";
+import { getAllCommunities } from "@/helpers/hSubscribeToComm";
 
 const AddNewPage = () => {
   const [image, setImage] = useState(null);
-  const [pickerValue, setPickerValue] = useState<string | null>("")
-  const [addTitle, setAddTitle] = useState<string | undefined>("")
-  const [addBody, setAddBody] = useState<string | undefined>("")
+  const [pickerValue, setPickerValue] = useState<string | null>(null);
+  const [postTitle, setPostTitle] = useState<string | undefined>("");
+  const [postBody, setPostBody] = useState<string | undefined>("");
+  const [postTags, setPostTags] = useState<string | undefined>("");
 
-  const auth = getAuth()
-  const currUser = auth.currentUser;
+  const [recipeName, setRecipeName] = useState<string | undefined>("");
+  const [recipeBody, setRecipeBody] = useState<string | undefined>("");
+
+  const [restaurantName, setRestaurantName] = useState<string | undefined>("");
+  const [restaurantAddress, setRestaurantAddress] = useState<
+    string | undefined
+  >("");
+
+  const [communityName, setCommunityName] = useState<string | undefined>("");
+  const [communityDesc, setCommunityDesc] = useState<string | undefined>("");
+
+  const [communityDropdown, setCommunityDropdown] = useState("");
+  const [communityDropdownItems, setCommunityDropdownItems] = useState([])
+
+  useEffect(() => {
+    const handleCommunityDropDownData = async () => {
+      if (pickerValue === "posts") {
+        const allData = await getAllCommunities();
+        const dropdownItems = allData.map(doc => ({
+          label: doc?.title,
+          value: doc.id,
+        }))
+        setCommunityDropdownItems(dropdownItems)
+      }
+    };
+    handleCommunityDropDownData();
+  }, [pickerValue]);
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -23,7 +57,7 @@ const AddNewPage = () => {
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
-      base64: true
+      base64: true,
     });
 
     if (!result.canceled) {
@@ -32,24 +66,147 @@ const AddNewPage = () => {
   };
 
   const handleUpload = async () => {
-    await pushSupabaseURL2Firebase(addTitle, addBody, image, pickerValue)
-  }
-
+    if (pickerValue === "posts")
+      await pushSupabaseURL2Firebase({
+        postTitle,
+        postBody,
+        communityDropdown,
+        image,
+        postTags,
+        pickerValue,
+      });
+    else if (pickerValue === "recipes")
+      await pushSupabaseURL2Firebase({
+        recipeName,
+        recipeBody,
+        image,
+        pickerValue,
+      });
+    else if (pickerValue === "restaurants")
+      await pushSupabaseURL2Firebase({
+        restaurantName,
+        restaurantAddress,
+        image,
+        pickerValue,
+      });
+    else if (pickerValue === "communities")
+      await pushSupabaseURL2Firebase({
+        communityName,
+        communityDesc,
+        pickerValue,
+      });
+    else return { error: "invalid choice" };
+  };
 
   return (
-    <SafeAreaView style={{paddingRight: 10, paddingLeft: 10, marginLeft: 15, marginRight: 15, marginVertical: 10}}>
-      <Text>Choose...</Text>   
-      <RNPickerSelect onValueChange={(value) => setPickerValue(value)} items={[
-        {label: "Post", value: "posts"},
-        {label: "Community", value: "communities"},
-        {label: "Recipe", value: "recipes"},
-        {label: "Restaurant", value: "restaurants"}
-      ]}/>
+    <SafeAreaView
+      style={{
+        paddingRight: 10,
+        paddingLeft: 10,
+        marginLeft: 15,
+        marginRight: 15,
+        marginVertical: 10,
+      }}
+    >
+      <Text>Choose...</Text>
+      <RNPickerSelect
+        onValueChange={(value) => setPickerValue(value)}
+        items={[
+          { label: "Post", value: "posts" },
+          { label: "Community", value: "communities" },
+          { label: "Recipe", value: "recipes" },
+          { label: "Restaurant", value: "restaurants" },
+        ]}
+      />
+      <SafeAreaView>
+        {pickerValue === "posts" ? (
+          <>
+            <RNPickerSelect
+              onValueChange={(value) => setCommunityDropdown(value)}
+              items={communityDropdownItems}
+              placeholder= {{
+                label: "Choose a community",
+                value: null
+              }}
+            />
+            <TextInput
+              placeholder="Title"
+              value={postTitle}
+              onChangeText={(text) => setPostTitle(text)}
+              style={{ visibility: "false" }}
+            />
+            <TextInput
+              placeholder="Body"
+              value={postBody}
+              onChangeText={(text) => setPostBody(text)}
+            />
+            <TextInput
+              placeholder="Tags (Space seperated)"
+              value={postTags}
+              onChangeText={(text) => setPostTags(text)}
+            />
+          </>
+        ) : null}
 
-      <TextInput placeholder="Title" value={addTitle} onChangeText={(text) => setAddTitle(text)} />
-      <TextInput placeholder="Body" value={addBody} onChangeText={(text) => setAddBody(text)} />
+        {pickerValue === "recipes" ? (
+          <>
+            <TextInput
+              placeholder="Recipe Name"
+              value={recipeName}
+              onChangeText={(text) => setRecipeName(text)}
+            />
+            <TextInput
+              placeholder="Recipe Body"
+              value={recipeBody}
+              onChangeText={(text) => setRecipeBody(text)}
+            />
+          </>
+        ) : null}
 
-      <TouchableOpacity onPress={() => pickImage()} style={{alignItems: "center", backgroundColor: "#f0f0f0", width: "50%", justifyContent: "center"}} ><Text>Upload image</Text></TouchableOpacity>
+        {pickerValue === "restaurants" ? (
+          <>
+            <TextInput
+              placeholder="Recipe Name"
+              value={restaurantName}
+              onChangeText={(text) => setRestaurantName(text)}
+            />
+            <TextInput
+              placeholder="Recipe Address"
+              value={restaurantAddress}
+              onChangeText={(text) => setRestaurantAddress(text)}
+            />
+          </>
+        ) : null}
+
+        {pickerValue === "communities" ? (
+          <>
+            <TextInput
+              placeholder="Community Name"
+              value={communityName}
+              onChangeText={(text) => setCommunityName(text)}
+            />
+            <TextInput
+              placeholder="Community Description"
+              value={communityDesc}
+              onChangeText={(text) => setCommunityDesc(text)}
+            />
+          </>
+        ) : null}
+
+        {pickerValue !== "communities" && pickerValue != null ? (
+          <TouchableOpacity
+            onPress={() => pickImage()}
+            style={{
+              alignItems: "center",
+              backgroundColor: "#f0f0f0",
+              width: "50%",
+              justifyContent: "center",
+            }}
+          >
+            <Text>Upload image</Text>
+          </TouchableOpacity>
+        ) : null}
+      </SafeAreaView>
 
       <Pressable onPress={() => handleUpload()}>
         <Text>Submit</Text>
--- helpers/hSupabaseImageHandler.tsx
+++ helpers/hSupabaseImageHandler.tsx
@@ -3,49 +3,122 @@ import { supabase } from "@/utils/supabase";
 import { decode } from "base64-arraybuffer";
 import * as FileSystem from "expo-file-system";
 import { getAuth } from "firebase/auth";
-import { addDoc, collection, query, where } from "firebase/firestore";
+import {
+  addDoc,
+  arrayUnion,
+  collection,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { useState } from "react";
 
-export const pushSupabaseURL2Firebase = async (
-  title,
-  body,
-  image,
-  pickerSelect
-) => {
+interface Props {
+  postTitle: string | undefined;
+  postBody: string;
+  postTags: string;
+  image: any;
+  pickerValue: string;
+  recipeName: string;
+  recipeBody: string;
+  restaurantName: string;
+  restaurantAddress: string;
+  communityName: string;
+  communityDesc: string;
+}
+
+export const pushSupabaseURL2Firebase = async (props) => {
   // handle pushing of publicURL in imageURL of firebase firestore
 
+  //   const [publicURL, setPublicURL] = useState<string | null>(null)
+
+  //   const {
+  //     postTitle,
+  //     postBody,
+  //     postTags,
+  //     image,
+  //     pickerValue,
+  //     recipeBody,
+  //     recipeName,
+  //     restaurantAddress,
+  //     restaurantName,
+  //     communityDesc,
+  //     communityName,
+  //   } = props;
+  console.log("got here");
   const auth = getAuth();
   const currUser = auth.currentUser;
 
   try {
     //push in supabase logic
-
-    const base64 = await FileSystem.readAsStringAsync(image?.uri, {
-      encoding: "base64",
-    });
-    const filePath = `${currUser?.uid}/${new Date().getTime()}.${
-      image?.type === "image" ? "png" : "mp4"
-    }`;
-    const contentType = image?.type === "image" ? "image/png" : "video/mp4";
-    const res = await supabase.storage
-      .from("community-app-assets")
-      .upload(filePath, decode(base64), { contentType });
-    const imagePublicURL = await supabase.storage
-      .from("community-app-assets")
-      .getPublicUrl(filePath);
-
-    const imgURL = imagePublicURL.data.publicUrl
-
-    // push in firestore logic
-    if (pickerSelect === "recipes") {
-      const upload2FirebaseRes = await addDoc(collection(db, pickerSelect), {
-        title,
-        desc: body,
-        imgURL
+    if (props.hasOwnProperty("image")) {
+      const base64 = await FileSystem.readAsStringAsync(props.image?.uri, {
+        encoding: "base64",
       });
-      console.log(upload2FirebaseRes)
-    }
+      const filePath = `${currUser?.uid}/${new Date().getTime()}.${
+        props.image?.type === "image" ? "png" : "mp4"
+      }`;
+      const contentType =
+        props.image?.type === "image" ? "image/png" : "video/mp4";
+      await supabase.storage
+        .from("community-app-assets")
+        .upload(filePath, decode(base64), { contentType });
+      const imagePublicURL = await supabase.storage
+        .from("community-app-assets")
+        .getPublicUrl(filePath);
+      console.log(imagePublicURL);
 
-    console.log(res);
+      const imgURL = imagePublicURL.data.publicUrl;
+      if (props.pickerValue === "recipes") {
+        const upload2FirebaseRes = await addDoc(collection(db, "recipes"), {
+          title: props.recipeName,
+          desc: props.recipeBody,
+          imgURL: imagePublicURL.data.publicUrl,
+        });
+        console.log(upload2FirebaseRes);
+      } else if (props.pickerValue === "posts") {
+        const upload2FirebaseRes = await addDoc(collection(db, "posts"), {
+          title: props.postTitle,
+          desc: props.postBody,
+          community: props.communityDropdown,
+          tags: [],
+          userID: currUser?.uid,
+          imageURL: imagePublicURL.data.publicUrl,
+        });
+        console.log(upload2FirebaseRes);
+      } else if (props.pickerValue === "restaurants") {
+        const upload2FirebaseRes = await addDoc(collection(db, "restaurants"), {
+          restaurantName: props.restaurantName,
+          address: props.restaurantAddress,
+          imgURL: imagePublicURL.data.publicUrl,
+        });
+        console.log(upload2FirebaseRes);
+      } else {
+        alert("invalid choice");
+      }
+    } else {
+      console.log("here");
+      const upload2FirebaseRes = await addDoc(collection(db, "communities"), {
+        title: props.communityName,
+        desc: props.communityDesc,
+        subscribedUsers: [currUser?.uid],
+        postsID: [],
+      });
+
+      // updating community in user's subscribed communities
+      console.log(upload2FirebaseRes.id);
+
+      const userRef = collection(db, "users");
+      const q = query(userRef, where("userId", "==", currUser?.uid));
+
+      const querySnapshot = await getDocs(q);
+      const userDataRef = querySnapshot.docs[0].ref;
+
+      await updateDoc(userDataRef, {
+        communities: arrayUnion(upload2FirebaseRes.id),
+      });
+    }
   } catch (e) {
     console.log(e, "err caught in catch");
   }
